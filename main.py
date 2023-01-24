598b15034690bccb03ea029657a622d13935b132
maccyaero <17176445+maccyaero@users.noreply.github.com>
2023-01-24 15:52:01 +1100

    Added JSON API Updated Blog Template to show Blog JSON Data.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+json_endpoint_url = " https://api.npoint.io/2f69e31b2a0c018e9f83"
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get(url=json_endpoint_url)
+    response_json = response.json()
+    print(response_json)
+    return render_template("index.html", blogs=response_json)
 
 
 if __name__ == "__main__":
